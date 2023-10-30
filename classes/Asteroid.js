324ee5ee856d6213e735105cbae5fdea8a6417a7
Karina <kbrilhante@gmail.com>
2023-10-30 15:37:10 -0300

    asteroids melhorados

--- classes/Asteroid.js
+++ classes/Asteroid.js
@@ -1,23 +1,30 @@
-class Asteroid {
-    constructor(x, y) {
-        this.sprite = new Sprite();
-        this.sprite.x = x;
-        this.sprite.y = y;
-        this.sprite.vel.x = -random(1, 3);
-        this.sprite.collider = "k";
-        this.sprite.diameter = 20;
-        this.sprite.spriteSheet = asteroidSS;
-        this.sprite.anis.frameSize = [64, 64];
-        // this.sprite.anis.frameDelay = 1;
-        this.sprite.addAnis({
-            a0: { row: 0, frames: 16 },
-            a1: { row: 1, frames: 16 },
-            a2: { row: 2, frames: 16 },
-            a3: { row: 3, frames: 16 },
-            a4: { row: 4, frames: 16 },
-            a5: { row: 5, frames: 16 }
+class Asteroids {
+    constructor() {
+        this.group = new Group();
+        this.group.collider = "k";
+        this.group.diameter = 64;
+        this.group.spriteSheet = asteroidSS;
+        this.group.anis.frameSize = [64, 64];
+        // this.group.anis.frameDelay = 1;
+        this.group.addAnis({
+            a0: { row: 0, frames: 1 },
+            a1: { row: 1, frames: 1 },
+            a2: { row: 2, frames: 1 },
+            a3: { row: 3, frames: 1 },
+            a4: { row: 4, frames: 1 },
+            a5: { row: 5, frames: 1 },
+            a6: { row: 6, frames: 1 },
+            a7: { row: 7, frames: 1 },
+            a8: { row: 8, frames: 1 }
         });
-        this.sprite.changeAni("a" + floor(random(5)));
-        this.sprite.anis.scale = 2;
+    }
+    create () {
+        let sprite = new this.group.Sprite();
+        sprite.x = random(width + this.group.diameter, 2 * width);
+        sprite.y = random(this.group.diameter, height - this.group.diameter);
+        sprite.vel.x = -random(1, 4);
+        sprite.changeAni("a" + floor(random(8)));
+        sprite.rotationSpeed = random([-1, 1]) * sprite.vel.x;
+        sprite.scale = random(0.3, 1);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let bgImg = [];
 let alienSS, asteroidSS;
 let bg, alien, edges, asteroids;
+let gameStart, gameOver;
 
 function preload() {
     bgImg.push(loadImage("./assets/star bg/1.png"));
@@ -8,52 +9,63 @@ function preload() {
     bgImg.push(loadImage("./assets/star bg/3.png"));
     bgImg.push(loadImage("./assets/star bg/4.png"));
     alienSS = loadImage("./assets/alienSanduiche.png");
-    asteroidSS = loadImage("./assets/asteroides.png");
+    asteroidSS = loadImage("./assets/asteroids64.png");
 }
 
 function setup() {
     new Canvas(bgImg[0].w / 2, bgImg[0].h / 2);
+
+    gameStart = false;
+    gameOver = false;
 
     bg = new BackgroundGroup(bgImg);
     edges = new Edges();
 
     alien = new Alien(alienSS);
 
-    asteroids = new Asteroid(width / 2, height / 2);
+    asteroids = new Asteroids();
 }
 
 function draw() {
     background("#fff");
-
-    bg.checkPosition();
-
-    alien.handleControls();
-
-    if (kb.pressed("z")) {
-        asteroids.sprite.changeAni("a0");
-    } else if (kb.pressed("x")) {
-        asteroids.sprite.changeAni("a1");
-    } else if (kb.pressed("c")) {
-        asteroids.sprite.changeAni("a2");
-    } else if (kb.pressed("v")) {
-        asteroids.sprite.changeAni("a3");
-    } else if (kb.pressed("b")) {
-        asteroids.sprite.changeAni("a4");
-    } else if (kb.pressed("n")) {
-        asteroids.sprite.changeAni("a5");
+    // if (kb.pressed("z")) {
+    //     asteroids.sprite.changeAni("a0");
+    // } else if (kb.pressed("x")) {
+    //     asteroids.sprite.changeAni("a1");
+    // } else if (kb.pressed("c")) {
+    //     asteroids.sprite.changeAni("a2");
+    // } else if (kb.pressed("v")) {
+    //     asteroids.sprite.changeAni("a3");
+    // } else if (kb.pressed("b")) {
+    //     asteroids.sprite.changeAni("a4");
+    // } else if (kb.pressed("n")) {
+    //     asteroids.sprite.changeAni("a5");
+    // }
+    if (gameStart) {
+        bg.checkPosition();
     }
+
+    if (!gameStart && !gameOver && kb.pressed('enter')) {
+        startGame();
+    }
+
+    if (gameStart && !gameOver) {
+        alien.handleControls();
+    }
+
 }
 
 function mousePressed() {
     allSprites.debug = true;
-    bg.move();
-    alien.startFlying();
+    asteroids.create()
 }
 
 function mouseReleased() {
     allSprites.debug = false;
 }
 
-function keyPressed() {
-
+function startGame() {
+    gameStart = true;
+    bg.move();
+    alien.startFlying();
 }
